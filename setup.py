8121579d9d2e4c77ab1bd7a26bc85bd4ce819a9d
Tom Usher <tom@tomusher.com>
2019-11-11 12:42:27 +0000

    Remove wagtail dependency

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, find_packages
 
-INSTALL_REQUIRES = [
-    'Wagtail<2.4',
-]
-
 TESTING_REQUIRES = [
 
 ]
@@ -14,7 +10,6 @@ setup(
     description="Smart quotes for Wagtail",
     author="Mike Monteith",
     author_email="<mike.monteith@nhs.net>",
-    install_requires=INSTALL_REQUIRES,
     packages=find_packages(),
     include_package_data=True,
     extras_require={'testing': TESTING_REQUIRES},
